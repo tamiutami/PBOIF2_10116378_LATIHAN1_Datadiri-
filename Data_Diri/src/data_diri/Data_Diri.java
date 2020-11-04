27d1870b6542768247bc78f054556a590a9df86a
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 02:33:55 +0700

    Add files via upload

---
+++ Data_Diri/src/data_diri/Data_Diri.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data_diri;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : IF-2
+ * Nim   : 10116378
+ */
+public class Data_Diri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA DIRI" + "\n" + "-------------------");
+      System.out.println("NAMA\t\t: Sri Utami Ningsih");
+      System.out.println("ALAMAT\t\t: Komplek Ambuleuit Blok w no 256");
+      System.out.println("TEMPAT LAHIR\t: Pandeglang , Banten");
+      System.out.println("TANGGAL LAHIR\t: 13 Agustus 1997");
+      System.out.println("--------------------------");
+      System.out.println("HOBI\t\t: Shopping ");
+      System.out.println("MAKES\t\t: Nasi Goreng ");
+      System.out.println("MIKES\t\t: Es Vanilla Latte");
+    }
+    
+}
